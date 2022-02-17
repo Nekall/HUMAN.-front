541f010f5689d77b7c6b5fdd7a319318f27c8dcf
Neka <nekoanchor@yahoo.fr>
2022-02-17 19:39:32 +0100

    style: init css

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar" >
-    <Link to="/">Home</Link>
-    <Link to="/concept">Concept</Link>
-    <Link to="/products">Shop</Link>
-    <Link to="/cart">Cart</Link>
+    <Link className="human" to="/">HUMAN.</Link>
+    <Link className="btn-pages" to="/concept">Concept</Link>
+    <Link className="btn-pages" to="/products">Shop</Link>
+    <Link className="btn-pages" to="/cart">Cart</Link>
   </nav>
 );
 
